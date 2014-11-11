ce3468cb4c028e6508218bba0de2830d4e93242b
Ravi Netravali <ravinet@mit.edu>
2014-11-11 18:13:21 -0500

    Add logging for document.getElementsByName

--- tracking_test/document.js
+++ tracking_test/document.js
@@ -3,8 +3,7 @@
                              document_handler);
     window.onload = function(){
         var contentDiv = document.getElementById("contentDiv");
-        //console.log(contentDiv);
-        //console.log(contentDiv.parentNode);
         contentDiv.innerHTML = "onload handler fired!";
+        var name = document.getElementsByName("testname");
     };
 })();
--- tracking_test/document_def.js
+++ tracking_test/document_def.js
@@ -8,6 +8,8 @@ var document_handler = {
                }
                
                var value = base[name];
+
+               // document.getElementById- log id, return value, and parents
                if(typeof(value) == "function"){
                    value = value.bind(base);
                    if(name == "getElementById"){
@@ -19,7 +21,37 @@ var document_handler = {
                                 parents.push(curr.parentNode.id);
                                 curr = curr.parentNode; 
                              }
-                             console.log( "getElementById(): id=" + id + "; return_value=" + retVal + "; parents:" + parents);
+                             console.log( "getElementById(): id=" + id + "; return_value=" + retVal + "=" + retVal.id + "; parents=" + parents);
+                             return retVal;
+                       };
+                       documentBindCache[name] = documentProxy;
+                       return documentProxy;
+                   }
+
+                   // document.getElementsByName- log name, return value, and for each node returned list node name and its parents
+                   if(name = "getElementsByName"){
+                       var documentProxy = function(id){
+                             var retVal = value(id);
+                             curr = retVal;
+                             parents = {};
+                             for (i = 0; i < retVal.length; i++) {
+                                 curr_parents = [];
+                                 curr = retVal[i];
+                                 while ( curr.parentNode != null && curr.parentNode.id != "" ) {
+                                     curr_parents.push(curr.parentNode.id);
+                                     curr = curr.parentNode;
+                                 }
+                                 parents[retVal[i].id] = curr_parents;
+                             }
+                             var parent_mappings = "";
+                             var nodes = "";
+                             for (var key in parents) {
+                                 nodes = nodes.concat(key +",");
+                                 parent_mappings = parent_mappings.concat(key + ":" + parents[key] + ", ");
+                             }
+                             parent_mappings = parent_mappings.slice(0, parent_mappings.length-2);
+                             nodes = nodes.slice(0, nodes.length-1);
+                             console.log( "getElementsByName(): name=" + id + "; return_value=" + retVal + "=" + nodes + "; parents=" + parent_mappings );
                              return retVal;
                        };
                        documentBindCache[name] = documentProxy;
