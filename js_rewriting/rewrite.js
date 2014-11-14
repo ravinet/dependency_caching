0a905d8ce967fbdda6f71fa3982fc8dae07efafd
Ameesh <ameeshg@gmail.com>
2014-11-14 11:34:16 -0500

    most of the rewriting

--- js_rewriting/rewrite.js
+++ js_rewriting/rewrite.js
@@ -1,20 +1,129 @@
-// initial source code (will change this to read from file)
-var tree = esprima.parse('var a = 0;');
+var code = "l=1; e = {}; f = 2; var h = 1; function m(a, b){ e.y = 8; a=1; k = 2; function n() { l = 2}}"
+console.log(code);
+var ast = esprima.parse(code, {loc: true});
+var scopeChain = []; // contains identifiers 
+var assignments = [];
 
-// json for anonymous function wrapper (note that innermost "body" is empty)
-var proxy_wrapper = {"type":"Program","body":[{"type":"ExpressionStatement","expression":{"type":"CallExpression","callee":{"type":"FunctionExpression","id":null,"params":[],"defaults":[],"body":{"type":"BlockStatement","body":[]},"rest":null,"generator":false,"expression":false},"arguments":[]}}]}
+estraverse.traverse(ast, {
+  enter: enter,
+  leave: leave
+});
 
-// take body from the initial source code and put into our new anonymous function
-var body = tree.body;
-// prepend previous source code with proxy declarations
-var window_proxy = {"type": "VariableDeclaration","declarations": [{"type": "VariableDeclarator","id": {"type": "Identifier","name": "window"},"init": {"type": "NewExpression","callee": {"type": "Identifier","name": "Proxy"},"arguments": [{"type": "Identifier","name": "_window"},{"type": "Identifier","name": "window_handler"}]}}],"kind": "var"};
-var document_proxy = {"type": "VariableDeclaration","declarations": [{"type": "VariableDeclarator","id": {"type": "Identifier","name": "document"},"init": {"type": "NewExpression","callee": {"type": "Identifier","name": "Proxy"},"arguments": [{"type": "Identifier","name": "_document"},{"type": "Identifier","name": "document_handler"}]}}],"kind": "var"};
-body.splice(0, 0, document_proxy);
-body.splice(0, 0, window_proxy);
-proxy_wrapper.body[0].expression.callee.body.body = body
+console.log(escodegen.generate(ast));
 
-//console.log(JSON.stringify(proxy_wrapper, "", 2));
+function createsNewScope(node){
+  return node.type === 'FunctionDeclaration' ||
+    node.type === 'FunctionExpression' ||
+    node.type === 'Program';
+}
 
-// convert the json back to javascript (should be same code wrapped in anonymous function)
-var new_js = escodegen.generate(proxy_wrapper);
-console.log(new_js);
+function enter(node){
+  if (createsNewScope(node)){
+    scopeChain.push([]);
+    if (node.params !== null) {
+      var currentScope = scopeChain[scopeChain.length - 1];
+      for (var i in node.params) {
+        currentScope.push(node.params[i]);
+      }
+    }
+  }
+
+  var currentScope = scopeChain[scopeChain.length - 1];
+  if (node.type === 'VariableDeclarator'){
+    currentScope.push(node.id);
+  }
+
+  if (node.type === 'AssignmentExpression'){
+    // if declared in global scope it's a global var
+    if (scopeChain.length === 1) {
+      currentScope.push(memberExpToIdentifier(node.left));
+    } else {
+      assignments.push(node);
+    }
+  }
+}
+
+function leave(node){
+  if (createsNewScope(node)){
+    checkForGlobals(assignments, scopeChain);
+    currentScope = scopeChain.pop();
+    printScope(currentScope, node);
+    assignments = [];
+    if (node.type === 'Program') {
+      for(var i in currentScope) {
+        rewriteAssignment(currentScope[i]);
+      }
+    }
+  }
+}
+
+function printScope(scope, node){
+  var varsDisplay = scopeToVarnames(scope).join(', ');
+  if (node.type === 'Program'){
+    console.log('Variables declared in the global scope:', 
+      varsDisplay);
+  }else{
+    if (node.id && node.id.name){
+      console.log('Variables declared in the function ' + node.id.name + '():',
+        varsDisplay);
+    }else{
+      console.log('Variables declared in anonymous function:',
+        varsDisplay);
+    }
+  }
+}
+
+function assignmentName(node) {
+  if (node.left.type === "Identifier") {
+    return node.left.name;
+  }
+  // ex: window.x = 1, this will return window
+  if (node.left.type === "MemberExpression") {
+    return node.left.object.name;
+  }
+}
+
+function checkForGlobals(assignments, scopeChain){
+  for (var i = 0; i < assignments.length; i++){
+    var assignment = assignments[i];
+    var varname = assignmentName(assignment);
+    if (!isVarDefined(varname, scopeChain)){
+      console.log('Global accessed', varname, 
+        'on line', assignment.loc.start.line, ':',
+        assignment.loc.start.column);
+      rewriteAssignment(assignment.left);
+    }
+  }
+}
+
+function rewriteAssignment(assignment) {
+  var node = memberExpToIdentifier(assignment);
+  node.name = "window." + node.name;
+}
+
+function memberExpToIdentifier(node) {
+  while(node.type !== "Identifier") {
+    node = node.object;
+  }
+  return node;  
+}
+
+function scopeToVarnames(scope) {
+  var output = [];
+  for (var i in scope) {
+    output.push(scope[i].name)
+  }
+  return output;
+}
+
+function isVarDefined(varname, scopeChain){
+  // start at 1 so we skip vars defined in global scope
+  for (var i = 1; i < scopeChain.length; i++){
+    var scope = scopeToVarnames(scopeChain[i]);
+    if (scope.indexOf(varname) !== -1){
+      return true;
+    }
+  }
+  return false;
+}
+
