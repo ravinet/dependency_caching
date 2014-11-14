6b06bae2c6600a2b6a7d7368c1199654d58ab30d
edethy <edethy@mit.edu>
2014-11-14 10:47:02 -0500

    handle multiple critical paths

--- critical_path.py
+++ critical_path.py
@@ -14,7 +14,12 @@ def get_critical_path(graph):
 		else:
 			slack_nodes[k] = {"node_earliest_value": node_earliest_value[k], "node_latest_value": node_latest_value[k], "slack_difference": node_latest_value[k]-node_earliest_value[k]}
 
-	return (critical_path_nodes, slack_nodes)
+
+	# Critical path nodes looks at all nodes where the earliest value equals the latest value.  It doesn't distinguish betweeen multiple critical paths
+
+	critical_path_list = get_critical_path_list(graph, node_earliest_value, node_latest_value);
+
+	return (critical_path_nodes, critical_path_list, slack_nodes)
 
 
 def get_earliest_values(subtree, depth):
@@ -37,6 +42,28 @@ def get_latest_values(subtree, max_depth):
 
 	node_latest_value = min([get_latest_values(x, max_depth) for x in children]) -1
 	return node_latest_value
+
+
+def get_critical_path_list(tree, earl_val_dict, late_val_dict):
+	
+	new_critical_paths = []
+
+	if earl_val_dict[tree.keys()[0]] != late_val_dict[tree.keys()[0]]:
+			return None
+
+	if tree.values()[0] is None:
+		return [tree.keys()]
+	else:
+		for val in tree.values()[0]:
+			new_node = tree.keys()
+			d = get_critical_path_list(val, earl_val_dict, late_val_dict)
+			if d is not None:
+				for l in d:
+					if l is not None:
+						new_critical_paths.append(new_node + l)
+
+		return new_critical_paths
+
 
 def get_latest_value_dict(subtree, max_depth):
 
