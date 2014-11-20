c0086b5f63b4152580ad0e32961865f4009b2561
Ravi Netravali <ravinet@mit.edu>
2014-11-20 09:16:28 -0500

    Move node logging to separate file

--- tracking_test/document.js
+++ tracking_test/document.js
@@ -1,9 +1,3 @@
-var _appendChild = Node.prototype.appendChild;
-Node.prototype.appendChild = function(child){
-                                 var retVal = _appendChild.call(this, child);
-                                 console.log("Call to Node.prototype.appendChild() in: arg=" + child + "= " + child.id + "; return_value=" + retVal);
-                                 return retVal;
-                             };
 (function(){
     var document = new Proxy(_document,
                              document_handler);
