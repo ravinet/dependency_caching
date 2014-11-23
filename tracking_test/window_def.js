9fd370d3f3a6196cf7adee90bbd22b6653b6485e
Ravi Netravali <ravinet@mit.edu>
2014-11-23 15:31:59 -0500

    Log calling script name

--- tracking_test/window_def.js
+++ tracking_test/window_def.js
@@ -1,8 +1,28 @@
+function get_caller(caller){
+    var script_attributes = "";
+    if ( caller == null ) {
+        script_attributes = "event handler or callback";
+    } else {
+        var attr = caller.attributes;
+        if( attr.length == 0 ) {
+            script_attributes = "inline script";
+        } else {
+            for(j=0; j < attr.length; j++) {
+                if( attr[j].name == "src" ) {
+                    script_attributes = script_attributes.concat( attr[j].value );
+                }
+            }
+        }
+    }
+    return script_attributes;
+}
+
 var _window = window;
 var windowBindCache = {};
 var window_handler = {
                   "get": function(base, name){
-                             console.log("READ: window- " + name);
+                             var caller = get_caller( document.currentScript);
+                             console.log("READ: window- " + name + "; from=" + caller);
                              if(name in windowBindCache){
                                  return windowBindCache[name];
                              }
@@ -15,7 +35,8 @@ var window_handler = {
                              return value;
                          },
                   "set": function(base, name, value){
-                             console.log("WRITE window- " + name + ": " + value);
+                             var caller = get_caller( document.currentScript);
+                             console.log("WRITE window- " + name + ":" + value + "; from=" + caller );
                              base[name] = value;
                              if(name in windowBindCache){
                                  delete windowBindCache[name];
