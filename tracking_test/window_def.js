bf021f0bb34aeca8938cdf63e81ee3017a0f50e5
Ravi Netravali <ravinet@mit.edu>
2014-12-05 14:41:26 -0500

    Window recursion to one level

--- tracking_test/window_def.js
+++ tracking_test/window_def.js
@@ -24,14 +24,40 @@ var window_handler = {
                              var caller = get_caller( document.currentScript);
                              var log = {"window": "READ", "var": name, "new_value": null, "script": caller}
                              console.log( JSON.stringify( log ) );
-                             if(name in windowBindCache){
-                                 return windowBindCache[name];
-                             }
-                             
                              var value = base[name];
-                             if(typeof(value) == "function"){
-                                 value = value.bind(base);
-                                 windowBindCache[name] = value;
+
+                             switch( typeof( value ) ){
+                                 case "number":
+                                 case "boolean":
+                                 case "string":
+                                 case "object":
+                                     if ( value == null ){
+                                         return value;
+                                     }
+                                     var object_handler = {
+                                         "get": function(base1, name){
+                                             var caller = get_caller( document.currentScript);
+                                             var log = {"window": "INNER READ", "var": name, "new_value": null, "script": caller}
+                                             console.log( JSON.stringify( log ) );
+                                             return value[name];
+                                         },
+                                         "set": function(base, name, new_value){
+                                             var caller = get_caller( document.currentScript);
+                                             var log = {'window': 'WRITE', 'var': name, 'new_value': value, 'script': caller};
+                                             console.log( JSON.stringify( log ) );
+                                             value[name] = new_value;
+                                         }
+                                     };
+                                     return (new Proxy( {}, object_handler));
+                                 case "function":
+                                     if(name in windowBindCache){
+                                         return windowBindCache[name];
+                                     }
+
+                                     if(typeof(value) == "function"){
+                                         value = value.bind(base);
+                                         windowBindCache[name] = value;
+                                     }
                              }
                              return value;
                          },
@@ -45,4 +71,3 @@ var window_handler = {
                              }
                          }
                  };
-
