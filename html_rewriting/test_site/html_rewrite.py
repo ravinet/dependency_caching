cb1758b961fdf9963c18c2c8f36560d285767118
edethy <edethy@mit.edu>
2014-12-08 08:45:30 -0500

    fine and replace html script tags

--- html_rewriting/test_site/html_rewrite.py
+++ html_rewriting/test_site/html_rewrite.py
@@ -1,29 +1,33 @@
 from bs4 import BeautifulSoup
 import pyesprima
+import json
 
 soup = BeautifulSoup(open("rewrite.html"))
 
-null = None
-false = False
+print soup
 
-proxy_wrapper = {"type":"Program","body":[{"type":"ExpressionStatement","expression":{"type":"CallExpression","callee":{"type":"FunctionExpression","id":null,"params":[],"defaults":[],"body":{"type":"BlockStatement","body":[]},"rest":null,"generator":false,"expression":false},"arguments":[]}}]}	
+proxy_wrapper = {"type":"Program","body":[{"type":"ExpressionStatement","expression":{"type":"CallExpression","callee":{"type":"FunctionExpression","id":None,"params":[],"defaults":[],"body":{"type":"BlockStatement","body":[]},"rest":None,"generator":False,"expression":False},"arguments":[]}}]}
 
 window_proxy = {"type": "VariableDeclaration","declarations": [{"type": "VariableDeclarator","id": {"type": "Identifier","name": "window"},"init": {"type": "NewExpression","callee": {"type": "Identifier","name": "Proxy"},"arguments": [{"type": "Identifier","name": "_window"},{"type": "Identifier","name": "window_handler"}]}}],"kind": "var"}
 
-document_proxy = {"type": "VariableDeclaration","declarations": [{"type": "VariableDeclarator","id": {"type": "Identifier","name": "document"},"init": {"type": "NewExpression","callee": {"type": "Identifier","name": "Proxy"},         "arguments": [{"type": "Identifier","name": "_document"},{"type": "Identifier","name": "document_handler"}]}}],"kind": "var"}
+document_proxy = {"type": "VariableDeclaration","declarations": [{"type": "VariableDeclarator","id": {"type": "Identifier","name": "document"},"init": {"type": "NewExpression","callee": {"type": "Identifier","name": "Proxy"}, "arguments": [{"type": "Identifier","name": "_document"},{"type": "Identifier","name": "document_handler"}]}}],"kind": "var"}
 
 
 for script in soup.find_all('script'):
 	body = pyesprima.parse(script.string);
+	print "\n"
+	print script.string
 
-	print body
 
 	prepend_doc_proxy = ''.join((str(document_proxy), str(body)))
 	prepend_window_proxy = ''.join((str(window_proxy), str(prepend_doc_proxy)))
 
-	print "\n final body"
-	print prepend_window_proxy
-
 	proxy_wrapper['body'][0]['expression']['callee']['body']['body'] = prepend_window_proxy
 
-	print proxy_wrapper
+	output = json.dumps(proxy_wrapper)
+
+	script.string = output
+
+#	print script.string
+
+print soup
