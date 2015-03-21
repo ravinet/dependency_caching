758bef7f22d609fa00d8ba1460e364d16eb65a56
Ameesh <ameeshg@gmail.com>
2015-03-21 18:03:17 -0400

    add attr to scripts with log so far

--- document_rewriting/html_parser.py
+++ document_rewriting/html_parser.py
@@ -4,18 +4,22 @@ import sys
 html_doc = sys.argv[1]
 
 soup = BeautifulSoup(open(html_doc))
-print soup.prettify()
 
-
+log = []
 def func(head, parent_path=[]):
   index = 0
   for child in head.children:
     if isinstance(child, element.Tag):
+      if child.name == "script":
+        child['log'] = ', '.join(log) 
+        del log[:]
       child_path = [index] + parent_path
       prop_name = "$$dom." + ".".join([str(num) for num in child_path])
       string_path = ", ".join([str(num) for num in child_path])
-      print {'OpType': 'WRITE', 'method': "null", 'PropName': prop_name, 'NodeProp': "null", 'id': "null", 'child_path': string_path, 'script': html_doc, "line": 0}
+      log.append(str({'OpType': 'WRITE', 'method': "null", 'PropName': prop_name, 'NodeProp': "null", 'id': "null", 'child_path': string_path, 'script': html_doc, "line": 0}))
       func(child, child_path)
       index += 1
 
 func(soup)
+
+print soup.prettify()
