1e043c6927ba0c86e15de6a97a15adbe3f7ec7e3
Ravi Netravali <ravinet@mit.edu>
2015-04-20 14:08:23 -0400

    preserve original domain in case request is not made right away

--- rewrite_image_tags.py
+++ rewrite_image_tags.py
@@ -1,8 +1,6 @@
 from bs4 import BeautifulSoup, element
 import sys
 
-for_ameesh = 'var logs_html = document.currentScript.getAttribute("htmllogs").split("\n");for ( j = 0; j < logs_html.length; j++ ) {window.js_rewriting_logs.push(logs_html[j]);}document.currentScript.parentNode.removeChild(document.currentScript);'
-
 html_doc = sys.argv[1]
 
 soup = BeautifulSoup(open(html_doc), "html5lib")
@@ -19,7 +17,7 @@ def func(head, parent_path=[]):
         if ( original_src != None and original_src != ""):
           child['src'] = ""
           new_script = soup.new_tag('script')
-          new_script.string = 'var req = new XMLHttpRequest;req.open("get", "' + original_src + '");if(document.currentScript.previousSibling instanceof Text){console.log("Text node between image tag and corresponding inline script!");}req.domref = document.currentScript.previousSibling;req.send();document.currentScript.parentNode.removeChild(document.currentScript);';
+          new_script.string = 'var req = new XMLHttpRequest();req.orig_domain = window.location.host;if(document.currentScript.previousSibling instanceof Text){console.log("Text node between image tag and corresponding inline script!");}req.domref = document.currentScript.previousSibling;req.open("GET", "' + original_src + '", false);req.send();document.currentScript.parentNode.removeChild(document.currentScript);';
           child.insert_after(new_script);
           func(child, child_path)
           index += 2
