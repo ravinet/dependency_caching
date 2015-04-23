59f0f2a10a52bd7e520f9444fdb8aa5a3318951f
Ravi Netravali <ravinet@mit.edu>
2015-04-23 12:49:02 -0400

    evaluate responses when possible and update global state

--- scheduler.js
+++ scheduler.js
@@ -2,9 +2,8 @@
 pending_queues = {};
 in_flight = {};
 
-// list parent and children (direct parents and children) URLs for each url
+// list parent URLs for each url
 parents = {};
-children = {};
 
 // number of urls which are later in the chain for each url
 depth = {};
@@ -15,50 +14,75 @@ evaluated = [];
 // requests which we have not yet evaluated because parent was not yet evaluated
 to_evaluate = [];
 
+// function to evaluate a response
+function evaluate_response(req) {
+    // if request has a domref property, re-assign the source
+    if ( req.hasOwnProperty("domref") ) {
+        req.domref.setAttribute("src", req.requested_url);
+    } else {
+        // from original JS so just execute default callback
+        req.onload_orig();
+    }
+
+    // add url to evaluated list
+    evaluated.append(validURL(req));
+}
+
 // function which recursively handles all to_eval requests based on what urls have already been evaluated
 function handle_to_eval () {
-
-};
-
-// global callback function for requests
-xhr_callback = function () {
-    curr_url = validURL(this);
-    eval_now = true;
-    // first check if we can eval it now (are all of its parents evaluated)
-    for ( j = 0; j < parents(curr_url); j++ ) {
-        if ( evaluted.indexOf(parents(curr_url)[j]) == -1 )
-            eval_now = false;
+    eval_index = []
+    for ( int q = 0; q < to_evaluate.length(); q++ ) {
+        if ( can_eval(to_evaluate[q]) ) {
+            evaluate_response(to_evaluate[q]);
+            eval_index.append(q);
         }
     }
 
-    if ( eval_now ) {
-        // if request has a domref property, re-assign the source
-        if ( this.hasOwnProperty("domref") ) {
-            this.domref.setAttribute("src", this.requested_url);
+    // remove all evaluated responses from the to_evaluate queue
+    first = true;
+    for ( i = 0; i < eval_index.length; i++) {
+        if ( first) {
+            to_evaluate.splice(eval_index[i],1);
+            first = false;
         } else {
-            // from original JS so just execute default callback
-            this.onload_orig();
+            to_evaluate.splice(eval_index[i]-1, 1);
         }
+    }
+};
 
-        // check the pending queue for this origin and make the next best request!
-        var ret = best_request(this.orig_location.origin);
-        if ( ret[0] ) {
+// function which checks if we can evaluate a specific response now
+function can_eval (req) {
+    curr_url = validURL(req);
+    for ( j = 0; j < parents(curr_url); j++ ) {
+        if ( evaluted.indexOf(parents(curr_url)[j]) == -1 )
+            return false;
+        }
+    }
+    // we can evaluate it now!
+    return true;
+}
+
+// global callback function for requests
+xhr_callback = function () {
+    if ( can_eval(this) ) { // we can eval it now!
+        evaluate_response(this);
+
+        // check the pending queue for this origin and make the next best request (as many as possible)!
+        var ret = best_request( this.orig_location.origin );
+        while ( ret[0] ) {
             var retVal = _xhrsend.call(ret[1]);
-            //!!!!still must update the list of current requests in-flight
+            in_flight[ret[1].orig_location.origin] = in_flight[ret[1].orig_location.origin] + 1;
+            ret = best_request( this.orig_location.origin );
         }
 
-        // handle as many to_eval requests as possible (should only be children this request or grandchildren, etc
         handle_to_eval();
-    } else {
-        // a parent is not yet evaluated so add this to the "to_eval" array
+    } else { // we cant eval it now
         to_evaluate.append(this);
     }
 };
 
 // function that decides whether or not to make a request based on the tree, pending lists, etc.
-// returns an array where first element is bool stating whether or not to make request now, second is the request to make
 function best_request(origin) {
-    // consult the number of pending requests to this origin and all requests in pending queue for this origin
     var best_req = "null";
     var best_req_index = "null";
     var curr_depth = 0;
@@ -111,8 +135,6 @@ function validURL(req) {
 
 var _xhrsend = XMLHttpRequest.prototype.send;
 XMLHttpRequest.prototype.send = function(){
-    // requests coming in have URL and location and async info
-
     // assign new callback while preserving original
     this.onload_orig = this.onload;
     this.onload = xhr_callback;
@@ -121,7 +143,7 @@ XMLHttpRequest.prototype.send = function(){
     if ( this.async == false ) {
         var retVal = _xhrsend.call(this);
         in_flight[this.orig_location.origin] = in_flight[this.orig_location.origin] + 1;
-        break;
+        return;
     }
 
     // always add incoming request to pending for the right origin
