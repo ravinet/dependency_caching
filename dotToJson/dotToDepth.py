8190c17695281fd9cbc117f08b9f9f7c2a76f8b8
Ameesh Goyal <ameesh@Ameeshs-MacBook-Air.local>
2015-04-25 19:46:17 -0400

    found depth of nodes

---
+++ dotToJson/dotToDepth.py
@@ -0,0 +1,37 @@
+import re, sys, urlparse
+import json
+from json import JSONEncoder
+
+import critical_path
+
+# takes in the dot file that we use (it ignores first three lines) and creates dictionary with key for each node and value for each as an array listing its children
+# for now, assumes that '/' is the root node
+dot = sys.argv[1]
+
+child_deps = {}
+unique_edges = []
+# must figure out what to do with two edges connecting the nodes (for now, include them in count)
+with open(dot) as f:
+    for line in f:
+        curr = line.strip("\n")
+        if "digraph G {" not in curr and curr != "ratio=compress;" and curr != "concentrate=true;" and curr != "}":
+            parent = curr.split(" ")[0]
+            child = curr.split("> ")[1].strip(";").strip("[color=red]")
+            if parent != child:
+                curr_edge = curr.strip(";").strip("[color=red]")
+                if curr_edge not in unique_edges:
+                    unique_edges.append(curr_edge)
+            if parent not in child_deps:
+                child_deps[parent] = [child]
+            else:
+                child_deps[parent].append(child)
+            if child not in child_deps:
+                child_deps[child] = []
+
+def depth(mappings, node):
+  if len(mappings[node]) == 0:
+    return 0
+  return max([depth(mappings, child) for child in mappings[node]]) + 1
+  
+depths = {node: depth(child_deps, node) for node in child_deps}
+print depths
