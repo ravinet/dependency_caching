931e18904430d5f203b3f62327c645798c4fa44a
Ravi Netravali <ravinet@mit.edu>
2015-07-13 14:37:32 -0700

    use dpkt to process tcpdump for timings

--- scheduler_timing.py
+++ scheduler_timing.py
@@ -3,6 +3,7 @@ import sys
 import time
 import signal
 import os 
+import dpkt
 
 site = sys.argv[1]
 FNULL = open(os.devnull, 'w')
@@ -18,3 +19,19 @@ time.sleep(8)
 os.system("sudo pkill -f -2 firefox")
 time.sleep(2)
 tcpdump.kill()
+time.sleep(3)
+
+f = open('test.pcap')
+pcap = dpkt.pcap.Reader(f)
+for ts, buf in pcap:
+    eth = dpkt.ethernet.Ethernet(buf)
+    if eth.type!=2048: #For ipv4, dpkt.ethernet.Ethernet(buf).type =2048        
+        continue
+    ip=eth.data
+    if ip.p!=6:
+        continue
+    tcp=ip.data
+    if tcp.dport == 80 and len(tcp.data) > 0:
+        http = dpkt.http.Request(tcp.data)
+        print http.uri
+f.close()
