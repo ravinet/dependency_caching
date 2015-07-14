aa827d36f42cdf8fece389026813ba2a83ee5c11
Ravi Netravali <ravinet@mit.edu>
2015-07-14 15:09:55 -0400

    handle cases where pcap has broken packet

--- scripts/scheduler_timing.py
+++ scripts/scheduler_timing.py
@@ -24,14 +24,19 @@ time.sleep(3)
 f = open('test.pcap')
 pcap = dpkt.pcap.Reader(f)
 for ts, buf in pcap:
-    eth = dpkt.ethernet.Ethernet(buf)
-    if eth.type!=2048: #For ipv4, dpkt.ethernet.Ethernet(buf).type =2048        
-        continue
-    ip=eth.data
-    if ip.p!=6:
-        continue
-    tcp=ip.data
-    if tcp.dport == 80 and len(tcp.data) > 0:
-        http = dpkt.http.Request(tcp.data)
-        print http.uri
+    try:
+        eth = dpkt.ethernet.Ethernet(buf)
+        if eth.type!=2048: #For ipv4, dpkt.ethernet.Ethernet(buf).type =2048
+            continue
+        ip=eth.data
+        if ip.p!=6:
+            continue
+        tcp=ip.data
+        if tcp.dport == 80 and len(tcp.data) > 0:
+            http = dpkt.http.Request(tcp.data)
+            print http.uri
+    except:
+        #print >> sys.stderr, "likely an incomplete entry in pcap file!"
+        pass
 f.close()
+os.system("sudo rm test.pcap")
