910bcfac40e7d797a391b15a4d89b0861b705b2b
Ravi Netravali <ravinet@mit.edu>
2015-07-14 14:30:44 -0700

    print page load time as time between last response and first request

--- scripts/scheduler_timing.py
+++ scripts/scheduler_timing.py
@@ -21,29 +21,38 @@ time.sleep(2)
 tcpdump.kill()
 time.sleep(3)
 
+first_response_time = ""
 responses = []
-
-f = open('test.pcap')
-pcap = dpkt.pcap.Reader(f)
-for ts, buf in pcap:
-    try:
-        eth = dpkt.ethernet.Ethernet(buf)
-        if eth.type!=2048: # check if ip packet
-            continue
-        ip=eth.data
-        if ip.p!=6: # check if tcp
-            continue
-        tcp=ip.data
-        if tcp.dport == 80 and len(tcp.data) > 0: # check if http request
-            http = dpkt.http.Request(tcp.data)
-            #print http.uri
-        if tcp.sport == 80: # this is a response
-            y = dpkt.http.Response(tcp.data)
-            #print y.status
-            responses.append(ts)
-    except:
-        pass
+try:
+    f = open('test.pcap')
+    pcap = dpkt.pcap.Reader(f)
+    for ts, buf in pcap:
+        try:
+            eth = dpkt.ethernet.Ethernet(buf)
+            if eth.type!=2048: # check if ip packet
+                continue
+            ip=eth.data
+            if ip.p!=6: # check if tcp
+                continue
+            tcp=ip.data
+            if tcp.dport == 80 and len(tcp.data) > 0: # check if http request
+                http = dpkt.http.Request(tcp.data)
+                if ( first_response_time == "" ):
+                    first_response_time = ts
+                #print http.uri
+            if tcp.sport == 80: # this is a response
+                y = dpkt.http.Response(tcp.data)
+                #print y.status
+                responses.append(ts)
+        except:
+            pass
+except:
+    print >> sys.stderr, "error reading pcap"
 f.close()
+print first_response_time
 print len(responses)
 print responses
+page_load_time = float(responses[len(responses)-1]) - float(first_response_time)
+# currently prints page load time in seconds
+print page_load_time
 os.system("sudo rm test.pcap")
