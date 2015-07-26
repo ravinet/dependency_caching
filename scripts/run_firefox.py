e91ee0bbdb9901ebbd4456f9ec82466a938a8631
Ravi Netravali <ravinet@mit.edu>
2015-07-26 07:48:23 -0700

    use xvfb displays when running firefox

--- scripts/run_firefox.py
+++ scripts/run_firefox.py
@@ -1,6 +1,10 @@
 import subprocess, sys, time, os
+from pyvirtualdisplay import Display
 
 site = sys.argv[1]
+
+display = Display(visible=0, size=(800,600))
+display.start()
 
 time.sleep(12)
 command = "firefox -private " + site + " > /dev/null 2>&1"
@@ -8,3 +12,4 @@ firefox = subprocess.Popen(command, shell=True)
 time.sleep(12)
 os.system("sudo pkill -f -2 firefox")
 time.sleep(6)
+display.stop()
